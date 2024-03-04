7781c0f1d15d0f281507009f9379ac625e3a2942
Davi-Ga <contato@daviga.dev.br>
2024-03-04 16:23:17 -0300

    hello from

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,8 +26,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`Aprendizado Documentado`}
+      description="O conhecimento é a sua melhor ferramenta">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
